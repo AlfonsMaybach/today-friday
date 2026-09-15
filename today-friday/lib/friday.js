b3d23158e5e5f160e555153c4055a5dd30dcdab4
Arenda_Payment <reklama.nado@gmail.com>
2026-09-16 02:27:18 +0300

    Delete today-friday directory

--- today-friday/lib/friday.js
+++
@@ -1,9 +0,0 @@
-export function isFridayNow() {
-  const timeZone = process.env.APP_TIMEZONE || 'Europe/Moscow';
-  const weekday = new Intl.DateTimeFormat('en-US', {
-    weekday: 'short',
-    timeZone
-  }).format(new Date());
-
-  return weekday === 'Fri';
-}
